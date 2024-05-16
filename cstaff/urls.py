312b611d3540f6c4210eeb479454205952992df5
EnjoyGray <vanya.konrad@gmail.com>
2024-05-16 11:12:57 +0200

    Add test view for foront + change model

---
+++ cstaff/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.urls import path
+from django.conf.urls.static import static
+from . import views
+
+
+app_name = 'cstaff'
+urlpatterns = [
+    path('', views.index, name="index")
+    # path('', views.IndexListView.as_view(), name="index"),    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cstaff/views.py
+++ cstaff/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# class StaffListView(ListView):
+#     model = Staff
+#     template_name = ".html"
+
+def index(request):
+    return render(request, "cstaff/index.html")
+   
